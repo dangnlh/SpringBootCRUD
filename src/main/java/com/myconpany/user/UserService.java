967228478f7e23891f127d73e85fd7f64658a1fc
nguyenlehaidang.290698@gmail.com <nguyenlehaidang.290698@gmail.com>
2021-12-27 04:18:16 +0700

    CRUD

--- src/main/java/com/myconpany/user/UserService.java
+++ src/main/java/com/myconpany/user/UserService.java
@@ -4,7 +4,9 @@ package com.myconpany.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,4 +17,23 @@ public class UserService {
         return (List<User>) repo.findAll();
     }
 
+    public void save(User user) {
+        repo.save(user);
+    }
+
+    public User get(Integer id) throws  UserNotFoundException {
+        Optional<User> result = repo.findById(id);
+        if(result.isPresent()){
+            return result.get();
+        }
+        throw new UserNotFoundException("Could not find any id with " +id);
+    }
+
+    public void delete (Integer id) throws UserNotFoundException {
+        Long count = repo.countById(id);
+        if(count ==null || count == 0){
+            throw new UserNotFoundException("Could not find any id with " +id);
+        }
+        repo.deleteById(id);
+    }
 }
